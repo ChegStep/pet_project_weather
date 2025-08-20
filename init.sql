765e1a797b28214c64b5272f90f9820b04d0b9f8
Stepan Chegodaev <72969219+ChegStep@users.noreply.github.com>
2025-08-20 13:53:34 +0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,10 +1,7 @@
--- Создаем схему если не существует
 CREATE SCHEMA IF NOT EXISTS weather;
 
--- Даем права пользователю
 GRANT ALL PRIVILEGES ON SCHEMA weather TO postgres;
 
--- Создаем таблицу для погоды
 CREATE TABLE IF NOT EXISTS weather.weather_data (
     city VARCHAR(100) NOT NULL,
     temp DECIMAL(5,2) NOT NULL,
@@ -14,6 +11,5 @@ CREATE TABLE IF NOT EXISTS weather.weather_data (
     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Даем права на таблицу
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA weather TO postgres;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA weather TO postgres;
